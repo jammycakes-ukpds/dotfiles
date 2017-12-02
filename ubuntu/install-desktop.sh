cf63eb0ad61ea02b417bf062993b07f95bc0abf8
James McKay <code@jamesmckay.net>
2017-12-02 12:41:58 +0000

    Add Enpass

--- ubuntu/install-desktop.sh
+++ ubuntu/install-desktop.sh
@@ -5,5 +5,14 @@
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+# Enpass
+
+echo "deb http://repo.sinew.in/ stable main" > \
+  /etc/apt/sources.list.d/enpass.list
+wget -O - https://dl.sinew.in/keys/enpass-linux.key | apt-key add -
+
+# All of the above
+
 apt-get update
-apt-get -y install code
+apt-get -y install code enpass
